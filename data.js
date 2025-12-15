37807a79f5852c60c012f1aec22b212db72f5111
ruchitaptp-lab <ruchit.a.ptp@gmail.com>
2025-12-15 21:49:10 +0530

    Update studentData with new student entries
    Replaced one student entry with multiple new entries, updating details for each student.

--- data.js
+++ data.js
@@ -8,11 +8,36 @@ const studentData = [
     remarks: "Excellent awareness and participation"
   },
   {
-    name: "Rohit Das",
-    mobile: "9123456789",
+    name: "ANIL KUMAR SAHOO",
+    mobile: "6372858042",
     class: "X",
-    grade: "C",
+    Percentage: "80%",
+    status: "PASS",
+    remarks:"Excellent awareness and participation"
+  },
+   {
+    name: "Muskan Sahoo",
+    mobile: "8328931082",
+    class: "X",
+    Percentage: "84",
+    status: "Pass",
+    remarks: "Excellent awareness and participation"
+  },
+   {
+    name: "ANSHUMAN TRIPATHY ",
+    mobile: "7008192749",
+    class: "X",
+    Percentage: "50%",
+    status: "PASS",
+    remarks: "Good awareness and participation"
+  },
+   {
+    name: "ANWESHA SAHU",
+    mobile: "6871644730",
+    class: "X",
+    percentage: "44%",
     status: "FAIL",
     remarks: "Needs improvement"
-  }
+  },
 ];
+
